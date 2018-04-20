66501b7e2880dba0936cb5ebdd8b1751f7379443
tax_k <taxfree9090@gmail.com>
2018-04-20 12:26:02 +0900

    one more thing commit

--- swiftFirstGit/ViewController.swift
+++ swiftFirstGit/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         print("view Did Load")
+        
+        print("one More Thing")
     }
 
     override func didReceiveMemoryWarning() {
